d6fe245da6483591f5deac567566991e9adae0ce
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-02-06 12:42:28 +0300

    Code cleanup

--- SoundGen/MainWindow.xaml.cs
+++ SoundGen/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 
 namespace SoundGen
 {
@@ -32,35 +20,30 @@ namespace SoundGen
 
         private void OpenFileBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "CSV Tables|*.csv|All files|*.*";
-            dlg.Multiselect = false;
-            var line = String.Empty;
-            if (dlg.ShowDialog().GetValueOrDefault(false))
+            var dlg = new OpenFileDialog {Filter = "CSV Tables|*.csv|All files|*.*", Multiselect = false};
+            if (!dlg.ShowDialog().GetValueOrDefault(false)) return;
+            FileNameLbl.Text = dlg.FileName;
+            var reader = new StreamReader(dlg.OpenFile(), Encoding.GetEncoding("windows-1251"));
+            string line;
+            while ((line = reader.ReadLine()) != null)
             {
-                FileNameLbl.Text = dlg.FileName;
-                StreamReader reader = new StreamReader(dlg.OpenFile(), Encoding.GetEncoding("windows-1251"));
-                while ((line = reader.ReadLine()) != null)
-                {
-                    LinesText.AppendText(line);
-                    LinesText.AppendText(Environment.NewLine);
-                }
-                reader.Close();
-
-                var waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(25000, 4);
-                WaveFileWriter waveFileWriter = new WaveFileWriter(dlg.FileName+".wav", waveFormat);
-                float amplitude = 1f;
-                float frequency = 1000;
-                
-                for (int n = 0; n < waveFileWriter.WaveFormat.SampleRate*4; n++)
-                {
-                    float sample = (float)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / waveFileWriter.WaveFormat.SampleRate));
-                    waveFileWriter.WriteSample(sample);
-                }
-                
-                waveFileWriter.Close();
-                
+                LinesText.AppendText(line);
+                LinesText.AppendText(Environment.NewLine);
             }
+            reader.Close();
+
+            var waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(25000, 4);
+            var waveFileWriter = new WaveFileWriter(dlg.FileName+".wav", waveFormat);
+            const float amplitude = 1f;
+            const float frequency = 1000;
+                
+            for (var n = 0; n < waveFileWriter.WaveFormat.SampleRate*4; n++)
+            {
+                var sample = (float)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / waveFileWriter.WaveFormat.SampleRate));
+                waveFileWriter.WriteSample(sample);
+            }
+                
+            waveFileWriter.Close();
         }
     }
 }
